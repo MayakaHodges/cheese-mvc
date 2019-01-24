a66ddf42828c4c48351acfb38f2ec4f0e466b6d6
Mayaka Hodges <mayakahodges@outlook.com>
2019-01-24 13:17:38 -0600

    Add Class 3.8 Prep work

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,90 +1,69 @@
 package org.launchcode.cheesemvc.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.launchcode.cheesemvc.modles.Cheese;
+import org.launchcode.cheesemvc.modles.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
+/**
+ * Created by LaunchCode
+ */
 
 @Controller
-//Every handler in classes must start with cheese
 @RequestMapping("cheese")
 public class CheeseController {
 
-    /*We used a ArrayList for just the cheese now we have added a description,
-    so we need a Hash Map.
-    static ArrayList<String> cheeses = new ArrayList<>();*/
-    HashMap<String, String> addForm = new HashMap<>();
-
-    //Request path: /cheese
+    // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        //Do not want to hard code the information, so we added an add method
-        // and moved ArraryList to class level.
-        //ArrayList<String> cheeses = new ArrayList<>();
-        //cheeses.add("cheddar");
-        //cheeses.add("parmesan");
-        //cheeses.add("munster");
-
-        //Sending information to the html view
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
-        model.addAttribute("addForm", addForm);
-        //Put the HTML into a folder named cheese.
-        //Returns the HTML index template
+
         return "cheese/index";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model)
-    {
+    public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
-    /*
-    The old way of processing forms, which are similar to Python's way
-    to do web applications. This way will still work
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(HttpServletRequest request)
-    {
-        String cheeseName = request.getParameter("cheeseName");
-    }
-    */
-
-    // Corresponds to the add.html
-    // @RequestParam looks in add.html to find the String variable.
-    @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription)
-    {
-        addForm.put(cheeseName,cheeseDescription);
-        //Old way of just adding cheese without a description attached.
-        //cheeses.add(cheeseName);
-        //Redirect to /cheese
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+        /*
+        * What ModelAttribute does:
+        * It is called model binding.
+        * Cheese newCheese = new Cheese();
+        * newCheese.setName(Request.getParameter("name"));
+        * newCheese.setDescription(Request.getParameter("description"));
+        * Behind the scene work.
+         */
+        CheeseData.add(newCheese);
         return "redirect:";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.GET)
-    public String displayRemoveCheeseForm(Model model)
-    {
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "Remove Cheese");
-        model.addAttribute("addForm", addForm);
         return "cheese/remove";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(@RequestParam Map<String, String> formData)
-    {
-        for(Map.Entry<String, String> formElement : formData.entrySet())
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int cheeseId : cheeseIds)
         {
-            addForm.remove(formElement.getKey());
+            CheeseData.remove(cheeseId);
         }
         return "redirect:";
     }
+
 }
